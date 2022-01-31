e0d76605cf9a58996f488c32083f927186d50b45
alecs396 <a.swainston16@gmail.com>
2022-01-31 11:48:20 -0700

    First commit. Screen works, no text yet

---
+++ typingTrainer.py
@@ -0,0 +1,36 @@
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Type Trainer")
+
+WHITE = (255, 255, 255)
+BLACK = (0 , 0, 0)
+FPS = 60
+
+def draw_text():
+    font = pygame.font.Font(None, 32)
+    text = font.render("Test",True, BLACK)
+    text_rect = text.get_rect()
+    
+    pygame.display.update()
+    
+    
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+        WIN.fill(WHITE)
+        pygame.display.update()
+        draw_text()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
