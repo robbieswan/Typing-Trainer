7353f046f4604755c9fd912cd27f42cac9434d38
Tyler242 <77693574+Tyler242@users.noreply.github.com>
2022-01-31 13:44:45 -0700

    screen will display

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from sys import exit
+
+pygame.init()
+
+
+# create display surface
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Typing Trainer')
+
+# clock object
+clock = pygame.time.Clock()
+
+
+# game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.fill('Black')
+
+    # update (reload) the screen every clock tick
+    pygame.display.update()
+
+    # clock tick frequency = 60fps
+    # refreshes about 60 times per second
+    clock.tick(60)
